4f31a23e07f4a97b1125d44d4f67b7efd3baea37
andrew <0x9171@gmail.com>
2020-10-20 00:16:29 +0300

    upd

--- banlist.go
+++ banlist.go
@@ -29,6 +29,12 @@ func (bl *BanList) UnBan(k uint64) {
 	bl.mu.Unlock()
 }
 
+func (bl *BanList) List() map[uint64]time.Time {
+	bl.mu.Lock()
+	defer bl.mu.Unlock()
+	return bl.keys
+}
+
 func (bl *BanList) IsBanned(k uint64) (expires *time.Time, ok bool) {
 	bl.mu.Lock()
 	if e, ok := bl.keys[k]; ok {
